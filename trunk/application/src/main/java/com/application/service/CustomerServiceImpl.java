42215224f829babf3433d46d4e77938d91e0f770
m.fajar01@gmail.com <m.fajar01@gmail.com@4df083d9-532f-179c-855e-66a4eb30ec0e>
2012-08-01 04:22:56 +0000

    space

--- trunk/application/src/main/java/com/application/service/CustomerServiceImpl.java
+++ trunk/application/src/main/java/com/application/service/CustomerServiceImpl.java
@@ -1,28 +1,28 @@
 package com.application.service;
- 
+
 import java.util.List;
 
 import com.application.dao.CustomerDao;
 import com.application.model.Customer;
- 
-public class CustomerServiceImpl implements CustomerService{
- 
+
+public class CustomerServiceImpl implements CustomerService {
+
 	CustomerDao customerDao;
- 
+
 	public void setCustomerDao(CustomerDao customerDao) {
 		this.customerDao = customerDao;
 	}
- 
-	public void save(Customer customer){ 
-		customerDao.save(customer); 
+
+	public void save(Customer customer) {
+		customerDao.save(customer);
 	}
- 
-	public List<Customer> findAllCustomer(){ 
+
+	public List<Customer> findAllCustomer() {
 		return customerDao.findAll();
 	}
 
 	@Override
-	public Customer searchCustomerById(long id){
+	public Customer searchCustomerById(long id) {
 		return customerDao.searchCustomerById(id);
 	}
 
